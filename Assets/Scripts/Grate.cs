7f2c95c644b0f1c4e393db5e8dde06bfa1becc28
Pacio0810 <samuuu686@gmail.com>
2024-11-05 16:45:18 +0100

    Fix GrateScript

--- Assets/Scripts/Grate.cs
+++ Assets/Scripts/Grate.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Grate : MonoBehaviour
@@ -5,18 +6,10 @@ public class Grate : MonoBehaviour
     [Header("Open Grate")]
     public Vector3 grateRotation;
     public Vector3 Offset;
-    
-    private Rigidbody _rigidbody;
-
-    private void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
 
     public void Activate()
     {
-        transform.position += Offset;
-        transform.rotation = Quaternion.Euler(grateRotation);
-        _rigidbody.useGravity = true;
+        transform.localPosition += Offset;
+        transform.localRotation = Quaternion.Euler(grateRotation);
     }
 }
